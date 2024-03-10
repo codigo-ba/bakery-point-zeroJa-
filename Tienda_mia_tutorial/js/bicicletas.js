6eeeae3a1301b0a8bc07d6d5a8656a06fc055e11
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2024-03-10 19:17:27 +0000

    Including Basis of JS. The folder Bicicletas is used to simulate a further backend...

---
+++ Tienda_mia_tutorial/js/bicicletas.js
@@ -0,0 +1,33 @@
+const bicicletas = [
+    {
+        id: 1,
+        nombre: "Ánfora Roja",
+        img: "./images/productos/Anfora_roja.jpg",
+        precio: 111111,
+    },
+    {
+        id: 2,
+        nombre: "Calle de París",
+        precio: 222222,
+    },
+    {
+        id: 3,
+        nombre: "Chaplin",
+        precio: 333333,
+    },
+    {
+        id: 4,
+        nombre: "La llegada",
+        precio: 444444,
+    },
+    {
+        id: 5,
+        nombre: "New Fig",
+        precio: 555555,
+    },
+    {
+        id: 6,
+        nombre: "Intensidad",
+        precio: 111111,
+    }
+]
