8708ba05cde7e121d169d2e26f7526977c841f23
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2024-04-15 23:28:36 +0000

    Figthing and winning with vars and tuples

--- classes/edades_sueldos.py
+++ classes/edades_sueldos.py
@@ -1,3 +1,7 @@
+#2) En una empresa se ingresan edades y sueldos correspondientes a una cierta cantidad de empleados ingresados
+#previamente.
+#Se desea conocer cuántos empleados tienen e/ 18 y 25 años, cuantos e/ 26 y 40, cuántos >40, y cuántos < de edad.
+#Se desea conocer el sueldo promedio.
 
 def main():
     contador_empleados = 0
@@ -13,7 +17,7 @@ def main():
         menor, e1, e2, mayor = edades
         sueldo_promedio = calcular_sueldo_promedio(contador_empleados, sueldo_acumulado)
         respuesta = preguntar_si_continua()
-    mostrar_resultados(edades, sueldo_promedio)
+    mostrar_resultados(menor, e1, e2, mayor, sueldo_promedio)
     print("Fin")
         
 
@@ -22,7 +26,7 @@ def solicitar_edad():
     return e
 
 def solicitar_sueldo():
-    s = int(input("Ingrese sueldo: ")) 
+    s = float(input("Ingrese sueldo: ")) 
     return s
 
 def evaluar_edad(e, menor, e1, e2, mayor):
@@ -37,10 +41,9 @@ def evaluar_edad(e, menor, e1, e2, mayor):
         mayor += 1
 
     return menor, e1, e2, mayor
-    #return f"Menores de 18: {menor}. Entre 18 y 25: {e1}. Entre 26 y 40: {e2}. Mayores de 40: {mayor}"
-
+    
 def calcular_sueldo_promedio(contador_empleados, sueldo_acumulado):
-    promedio = sueldo_acumulado/contador_empleados
+    promedio = round(sueldo_acumulado/contador_empleados, 2)
     return promedio
 
 def preguntar_si_continua():
@@ -49,8 +52,8 @@ def preguntar_si_continua():
         respuesta = input("Debe ingresar S o N: ")
     return respuesta
 
-def mostrar_resultados(edades, sueldo_promedio):
-    print(edades)
-    print(f"El sueldo promedio es: {sueldo_promedio}")
+def mostrar_resultados(menor, e1, e2, mayor, sueldo_promedio):
+    print(f"Menores de 18: {menor}. Entre 18 y 25: {e1}. Entre 26 y 40: {e2}. Mayores de 40: {mayor}")
+    print(f"El sueldo promedio es: ${sueldo_promedio}")
 
 main()
