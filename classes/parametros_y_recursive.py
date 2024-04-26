ff336a7345d53bcdc8aeba63920701ee2114fe87
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2024-04-26 23:46:21 +0000

    Adding knowledge to recursive

--- classes/parametros_y_recursive.py
+++ classes/parametros_y_recursive.py
@@ -43,3 +43,21 @@ f(1)
 print()
 f(-3)
 print()
+
+
+#◘◘◘Ejemplo: Contruir una f: que haga una pregunta y acepte 3 intentos usando recursividad
+
+
+def jugar(intento):
+    respuesta = int(input("Adiviná qué número estoy pensando:" ))
+    if respuesta != 7:
+        if intento < 3:
+            print("Nop!!! Intentálo de nuevo...")
+            intento += 1  #intento es un param. Se puede actualizar como una var cualquiera
+            jugar(intento)
+        else:
+            print("Sorry!")
+    else:
+        print("Ganaste!!")
+
+jugar(1)      
